b008f7f69dd190e5dc7fb59d64d1e9ee6ac1181d
Wim Vandersmissen <wim.vandersmissen@nascom.be>
2017-04-03 13:22:20 +0200

    Fix postfix smtp service not found & DNS issue

--- postfix/run.sh
+++ postfix/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#touch /var/log/mail.log
+FILES="localtime services resolv.conf hosts nsswitch.conf nss_mdns.config"
+for file in $FILES; do
+    cp /etc/${file} /var/spool/postfix/etc/${file}
+    chmod a+rX /var/spool/postfix/etc/${file}
+done
 rsyslogd
 echo Syslog started
 postfix start
