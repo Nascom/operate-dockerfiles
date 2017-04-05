065a70d5577cd5b18a36c8375bf7dd6691436774
Wim Vandersmissen <wim.vandersmissen@nascom.be>
2017-04-05 16:29:15 +0200

    Reorganise php containers for easier maintenance

--- php/7.0/apache/run.sh
+++ php/7.0/apache/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 rm -f /var/run/apache2/apache2.pid
-sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/apache2/sites-available/000-default.conf
-sed -i "s|ENVIRONMENT_VAR|$ENVIRONMENT|g" /etc/apache2/sites-available/000-default.conf
 sed -i "s|PHP_ERROR_REPORTING_VAR|$PHP_ERROR_REPORTING|g" /etc/php/7.0/apache2/conf.d/php.ini
 sed -i "s|PHP_ERROR_REPORTING_VAR|$PHP_ERROR_REPORTING|g" /etc/php/7.0/cli/conf.d/php.ini
 sed -i "s|%SMTP_SERVER%|$SMTP_SERVER|g" /etc/ssmtp/ssmtp.conf
