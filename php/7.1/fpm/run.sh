cc8013183bfc3e55b8245183af750da6523b6395
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2017-11-07 15:22:07 +0100

    Add experimental support to enforce FPM UID

--- php/7.1/fpm/run.sh
+++ php/7.1/fpm/run.sh
@@ -10,5 +10,13 @@ done
 sed -i "s|%SMTP_SERVER%|$SMTP_SERVER|g" /etc/ssmtp/ssmtp.conf
 sed -i "s|%ROOT_EMAIL%|$ROOT_EMAIL|g" /etc/ssmtp/ssmtp.conf
 
+if id "$PHP_FPM_USER" >/dev/null 2>&1; then
+    # User exists, change UID
+    usermod -u $PHP_FPM_UID $PHP_FPM_USER
+else
+    # User does not exist, create it
+    useradd -u $PHP_FPM_UID -r -m -s /bin/bash -c "FPM user" $PHP_FPM_USER
+fi
+
 mkdir -p /run/php
 /usr/sbin/php-fpm7.1 --allow-to-run-as-root -c /etc/php/7.1/fpm --nodaemonize
