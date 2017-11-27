5df9988b4f47b3bb99bd75cd4e46a2636f5c1a66
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2017-11-27 11:34:00 +0100

    Add support to run fpm with a specific UID

--- php/5.3/fpm/run.sh
+++ php/5.3/fpm/run.sh
@@ -10,4 +10,12 @@ done
 sed -i "s|%SMTP_SERVER%|$SMTP_SERVER|g" /etc/ssmtp/ssmtp.conf
 sed -i "s|%ROOT_EMAIL%|$ROOT_EMAIL|g" /etc/ssmtp/ssmtp.conf
 
+if id "$PHP_FPM_USER" >/dev/null 2>&1; then
+    # User exists, change UID
+    usermod -u $PHP_FPM_UID $PHP_FPM_USER
+else
+    # User does not exist, create it
+    useradd -u $PHP_FPM_UID -r -m -s /bin/bash -c "FPM user" $PHP_FPM_USER
+fi
+
 /usr/sbin/php5-fpm -c /etc/php5/fpm
