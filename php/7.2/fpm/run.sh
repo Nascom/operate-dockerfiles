3c3f8221d97956f2134018513e566a85b23eddb1
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2017-11-30 14:52:12 +0100

    Add PHP 7.2 support

---
+++ php/7.2/fpm/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+variables=( "PHP_FPM_USER" "PHP_FPM_PORT" "PHP_ERROR_REPORTING" "ENVIRONMENT" )
+
+for var in "${variables[@]}"
+do
+   :
+   sed -i "s|%$var%|${!var}|g" /etc/php/7.2/fpm/php-fpm.conf
+done
+sed -i "s|%SMTP_SERVER%|$SMTP_SERVER|g" /etc/ssmtp/ssmtp.conf
+sed -i "s|%ROOT_EMAIL%|$ROOT_EMAIL|g" /etc/ssmtp/ssmtp.conf
+
+if id "$PHP_FPM_USER" >/dev/null 2>&1; then
+    # User exists, change UID
+    usermod -u $PHP_FPM_UID $PHP_FPM_USER
+else
+    # User does not exist, create it
+    useradd -u $PHP_FPM_UID -r -m -s /bin/bash -c "FPM user" $PHP_FPM_USER
+fi
+
+mkdir -p /run/php
+/usr/sbin/php-fpm7.2 --allow-to-run-as-root -c /etc/php/7.2/fpm --nodaemonize
