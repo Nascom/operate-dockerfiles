93aff60e67d25df68fda647972d862b26d843f23
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2018-01-29 16:35:11 +0100

    Always run as www-data:www-data in the container, but allow changing user & group by setting PHP_FPM_UID & PHP_FPM_GID

--- php/7.2/fpm/run.sh
+++ php/7.2/fpm/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-variables=( "PHP_FPM_USER" "PHP_FPM_PORT" "PHP_ERROR_REPORTING" "ENVIRONMENT" )
+variables=( "PHP_FPM_PORT" "PHP_ERROR_REPORTING" "ENVIRONMENT" )
 
 for var in "${variables[@]}"
 do
    :
-   sed -i "s|%$var%|${!var}|g" /etc/php/7.2/fpm/php-fpm.conf
+   sed -i "s|%$var%|${!var}|g" /etc/php5/fpm/php-fpm.conf
 done
 sed -i "s|%SMTP_SERVER%|$SMTP_SERVER|g" /etc/ssmtp/ssmtp.conf
 sed -i "s|%ROOT_EMAIL%|$ROOT_EMAIL|g" /etc/ssmtp/ssmtp.conf
 
-if id "$PHP_FPM_USER" >/dev/null 2>&1; then
-    # User exists, change UID
-    usermod -u $PHP_FPM_UID $PHP_FPM_USER
-else
-    # User does not exist, create it
-    useradd -u $PHP_FPM_UID -r -m -s /bin/bash -c "FPM user" $PHP_FPM_USER
+if [ ! -z $PHP_FPM_GID ]; then
+    groupmod -g $PHP_FPM_GID www-data
+fi
+
+if [ ! -z $PHP_FPM_UID ]; then
+    usermod -u $PHP_FPM_UID www-data
 fi
 
 mkdir -p /run/php
