fc2c7b47a1a3a1402dad26cd30671d185d154b77
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2018-12-06 18:08:41 +0100

    Test with Salt containers ...

---
+++ salt/api/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/salt-api -c /etc/salt --log-file-level=quiet --log-level=debug
---
+++ salt/master/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if ! (( $(id "$SALT_API_USER" > /dev/null 2>&1) )); then
+    /usr/sbin/useradd -ms /bin/false $SALT_API_USER
+    echo "$SALT_API_USER:$SALT_API_PASSWORD" | /usr/sbin/chpasswd
+fi
+/usr/bin/salt-master -c /etc/salt --log-file-level=quiet --log-level=debug
