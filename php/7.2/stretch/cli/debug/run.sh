22daf1a95be31ccd9d533faa5aba9cc15f51c49c
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2018-12-13 16:33:10 +0100

    Remove debug containers (just compile custom ones with xdebug enabled in docker-compose.yml if you need them)

--- php/7.2/stretch/cli/debug/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sed -i "s|%SMTP_SERVER%|$SMTP_SERVER|g" /etc/ssmtp/ssmtp.conf
-sed -i "s|%ROOT_EMAIL%|$ROOT_EMAIL|g" /etc/ssmtp/ssmtp.conf
-
-sed -i "s|%XDEBUG_PORT%|$XDEBUG_PORT|g" /etc/php/7.2/cli/conf.d/20-xdebug.ini
-
-/usr/bin/php "$@"
